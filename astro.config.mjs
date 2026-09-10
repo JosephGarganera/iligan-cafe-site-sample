013204f335ab28515189c3957ac9e62dbdd450ce
JosephGarganera <jag.garganera@gmail.com>
2026-09-10 19:35:27 +0800

    chore: convert app architecture to live on-demand server-side rendering mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import vercel from "@astrojs/vercel";
 import tailwind from "@astrojs/tailwind"; // Import the official stable integration
 
 export default defineConfig({
-  output: "static",
+  output: "server",
 
   adapter: vercel({
     webAnalytics: { enabled: true },
